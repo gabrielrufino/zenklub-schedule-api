84e263d0f6c954dd09401f39ef72c9fe7464e839
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 19:37:27 -0300

    Validators for availability creation

---
+++ src/middlewares/validators/availabilities.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+import { body, validationResult } from 'express-validator'
+
+const validators = {
+  post: [
+    body('professional').isString(),
+    body('startDate').isDate(),
+    body('startTime').matches(/^(10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|[1-9]):[0-5][0-9]$/),
+    body('endDate').isDate(),
+    body('endTime').matches(/^(10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|[1-9]):[0-5][0-9]$/),
+    (request: Request, response: Response, next: NextFunction) => {
+      const errors = validationResult(request)
+      if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() })
+      }
+
+      return next()
+    }
+  ]
+}
+
+export default validators
