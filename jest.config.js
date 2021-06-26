b30a0730269318fba0ba1651c76284d1bd95be69
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 20:29:26 -0300

    'resetMocks: true' at jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  resetMocks: true,
   moduleNameMapper: {
     '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
     '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1'
