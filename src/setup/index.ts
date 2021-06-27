fda77f7b568ea125fcea180bedcc1ceb444346bb
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 21:19:57 -0300

    Using environment variables to mongodb connection

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -4,10 +4,15 @@ import dotenv from 'dotenv'
 export default async () => {
   dotenv.config()
 
-  const { MONGO_URL = 'mongodb://localhost:27017/zenklub_schedule' } = process.env
+  const {
+    MONGO_URL = 'mongodb://localhost:27017/zenklub_schedule',
+    MONGO_DATABASE_NAME = 'zenklub_schedule'
+  } = process.env
 
-  mongoose.connect(MONGO_URL, {
+  await mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    dbName: MONGO_DATABASE_NAME
   })
 }
