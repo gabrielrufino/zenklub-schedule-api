dd0e609fc3abc03f97444019c665f3a96d111ea1
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 21:22:14 -0300

    Route to create availability

---
+++ src/routers/availabilities.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+import controllers from '@controllers/availabilities'
+import validators from '@middlewares/validators/availabilities'
+
+const router = Router()
+
+router.post('/', validators.post, controllers.post)
+
+export default router
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import root from './root'
+import availabilities from './availabilities'
 
 const router = Router()
 
 router.use('/', root)
+router.use('/availabilities', availabilities)
 
 export default router
