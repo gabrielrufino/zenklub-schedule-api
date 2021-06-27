e7ad69960caa237f9998fd0492eabe97af028fa4
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 21:19:24 -0300

    Removing autoclear

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  resetMocks: true,
   moduleNameMapper: {
     '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
     '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
