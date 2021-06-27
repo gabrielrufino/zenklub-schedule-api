f9e19c5de391793499a6e593fe71101d87c8715d
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 23:26:16 -0300

    Using not found and error middlewares

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
 
+import notFoundMiddleware from '@middlewares/not-found'
+import errorMiddleware from '@middlewares/error'
+
 import root from './root'
 import availabilities from './availabilities'
 
@@ -7,5 +10,8 @@ const router = Router()
 
 router.use('/', root)
 router.use('/availabilities', availabilities)
+
+router.use(notFoundMiddleware)
+router.use(errorMiddleware)
 
 export default router
