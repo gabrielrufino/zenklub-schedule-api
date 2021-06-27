95774134b7a4077db235301313474d9332c5c947
Gabriel Rufino <contato@gabrielrufino.com>
2021-06-26 23:24:51 -0300

    moduleNameMapper for exceptions

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@exceptions/(.*)$': '<rootDir>/src/exceptions/$1',
     '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
     '^@models/(.*)$': '<rootDir>/src/models/$1'
   }
